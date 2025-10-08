97616844255e3da262eff77763b636078361cffe
Delisa0 <elahidelisa0@gmail.com>
2025-10-08 21:08:10 +1100

    time 1

---
+++ activity 1.py
@@ -0,0 +1,6 @@
+def printnumber(n):
+    literation=0
+    print("the total literation done by computer is:",literation,"\n")
+printnumber(10)
+printnumber(200)
+print("\n with any 'n' the time taken by the computer will not change")
